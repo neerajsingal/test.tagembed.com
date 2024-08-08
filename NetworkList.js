4ab193441eb6fb2aef120375c03a389270169f3d
kamrantaggbox <kamran@taggbox.com>
2024-08-08 18:21:44 +0530

    update

---
+++ NetworkList.js
@@ -0,0 +1,222 @@
+export const NETWORK_LIST_API =[
+    {
+      "id": 23,
+      "name": "Airbnb",
+      "icon": "fa-airbnb",
+      "description": "Airbnb",
+      "status": 1,
+      "color": "#FF7977",
+      "createdAt": "2020-04-16T11:58:28.000Z",
+      "updatedAt": "2020-04-16T11:58:28.000Z"
+    },
+    {
+      "id": 3,
+      "name": "Facebook",
+      "icon": "fa-facebook",
+      "description": "Facebook",
+      "status": 1,
+      "color": "#3e5c9a",
+      "createdAt": "2018-10-25T06:11:03.000Z",
+      "updatedAt": "2018-10-25T06:11:03.000Z"
+    },
+    {
+      "id": 6,
+      "name": "Flickr",
+      "icon": "fa-flickr",
+      "description": "Flickr",
+      "status": 1,
+      "color": "#006ADD",
+      "createdAt": "2018-10-25T06:11:03.000Z",
+      "updatedAt": "2018-10-25T06:11:03.000Z"
+    },
+    {
+      "id": 26,
+      "name": "Giphy",
+      "icon": "fa-giphy",
+      "description": "Giphy",
+      "status": 1,
+      "color": "#000000",
+      "createdAt": "2020-04-25T12:18:43.000Z",
+      "updatedAt": "2020-04-25T12:18:43.000Z"
+    },
+    {
+      "id": 4,
+      "name": "Google Review",
+      "icon": "fa-google",
+      "description": "Google",
+      "status": 1,
+      "color": "#4081ED",
+      "createdAt": "2018-10-25T06:11:03.000Z",
+      "updatedAt": "2018-10-25T06:11:03.000Z"
+    },
+    {
+      "id": 2,
+      "name": "Instagram",
+      "icon": "fa-instagram",
+      "description": "Instagram",
+      "status": 1,
+      "color": "#d45662",
+      "createdAt": "2018-10-25T06:11:03.000Z",
+      "updatedAt": "2018-10-25T06:11:03.000Z"
+    },
+    {
+      "id": 18,
+      "name": "Instagram Business",
+      "icon": "fa-instagram",
+      "description": "Instagram Business",
+      "status": 1,
+      "color": "#cd486b",
+      "createdAt": "2018-10-25T06:11:03.000Z",
+      "updatedAt": "2018-10-25T06:11:03.000Z"
+    },
+    {
+      "id": 10,
+      "name": "LinkedIn",
+      "icon": "fa-linkedin",
+      "description": "LinkedIn",
+      "status": 1,
+      "color": "#0177B5",
+      "createdAt": "2018-10-25T06:11:03.000Z",
+      "updatedAt": "2018-10-25T06:11:03.000Z"
+    },
+    {
+      "id": 29,
+      "name": "OnSiteUpload",
+      "icon": "fa-onsite-upload",
+      "description": "OnSiteUpload",
+      "status": 1,
+      "color": "#613983",
+      "createdAt": "2021-09-30T11:49:05.000Z",
+      "updatedAt": "2021-09-30T11:49:05.000Z"
+    },
+    {
+      "id": 5,
+      "name": "Pinterest",
+      "icon": "fa-pinterest",
+      "description": "Pinterest",
+      "status": 1,
+      "color": "#E60022",
+      "createdAt": "2018-10-25T06:11:03.000Z",
+      "updatedAt": "2018-10-25T06:11:03.000Z"
+    },
+    {
+      "id": 12,
+      "name": "RSS",
+      "icon": "fa-rss",
+      "description": "RSS",
+      "status": 1,
+      "color": "#f78422",
+      "createdAt": "2018-10-25T06:11:03.000Z",
+      "updatedAt": "2018-10-25T06:11:03.000Z"
+    },
+    {
+      "id": 20,
+      "name": "Slack",
+      "icon": "fa-slack",
+      "description": "Slack",
+      "status": 1,
+      "color": "#510d4e",
+      "createdAt": "2018-12-05T13:03:32.000Z",
+      "updatedAt": "2018-12-05T13:03:32.000Z"
+    },
+    {
+      "id": 25,
+      "name": "SoundCloud",
+      "icon": "fa-soundcloud",
+      "description": "SoundCloud",
+      "status": 0,
+      "color": "#FC5502",
+      "createdAt": "2020-04-25T12:18:43.000Z",
+      "updatedAt": "2020-04-25T12:18:43.000Z"
+    },
+    {
+      "id": 28,
+      "name": "TikTok",
+      "icon": "fa-tiktok",
+      "description": "tiktok",
+      "status": 1,
+      "color": " #000000",
+      "createdAt": "2021-09-04T09:27:30.000Z",
+      "updatedAt": "2021-09-04T09:27:30.000Z"
+    },
+    {
+      "id": 11,
+      "name": "Tumblr",
+      "icon": "fa-tumblr",
+      "description": "Tumblr",
+      "status": 1,
+      "color": "#36465d",
+      "createdAt": "2018-10-25T06:11:03.000Z",
+      "updatedAt": "2018-10-25T06:11:03.000Z"
+    },
+    {
+      "id": 1,
+      "name": "Twitter",
+      "icon": "fa-twitter",
+      "description": "Twitter",
+      "status": 1,
+      "color": "#29a9e1",
+      "createdAt": "2018-10-25T06:11:03.000Z",
+      "updatedAt": "2018-10-25T06:11:03.000Z"
+    },
+    {
+      "id": 8,
+      "name": "Vimeo",
+      "icon": "fa-vimeo-square",
+      "description": "Vimeo",
+      "status": 1,
+      "color": "#1eb8eb",
+      "createdAt": "2018-10-25T06:11:03.000Z",
+      "updatedAt": "2018-10-25T06:11:03.000Z"
+    },
+    {
+      "id": 30,
+      "name": "VKontakte",
+      "icon": "fa-vk",
+      "description": "VKontakte",
+      "status": 1,
+      "color": "#0077ff",
+      "createdAt": "2021-10-13T06:50:24.000Z",
+      "updatedAt": "2021-10-13T06:50:24.000Z"
+    },
+    {
+      "id": 15,
+      "name": "Workplace",
+      "icon": "fa-workplace",
+      "description": "Workplace",
+      "status": 1,
+      "color": "#4767AB",
+      "createdAt": "2018-10-25T06:11:03.000Z",
+      "updatedAt": "2018-10-25T06:11:03.000Z"
+    },
+    {
+      "id": 21,
+      "name": "Yammer",
+      "icon": "fa-yammer",
+      "description": "Yammer",
+      "status": 1,
+      "color": "#0078D4",
+      "createdAt": "2020-01-16T08:24:26.000Z",
+      "updatedAt": "2020-01-16T08:24:26.000Z"
+    },
+    {
+      "id": 19,
+      "name": "Yelp",
+      "icon": "fa-yelp",
+      "description": "Yelp",
+      "status": 1,
+      "color": "#be362e",
+      "createdAt": "2018-11-14T09:04:28.000Z",
+      "updatedAt": "2018-11-14T09:04:28.000Z"
+    },
+    {
+      "id": 7,
+      "name": "YouTube",
+      "icon": "fa-youtube",
+      "description": "YouTube",
+      "status": 1,
+      "color": "#FF0000",
+      "createdAt": "2018-10-25T06:11:03.000Z",
+      "updatedAt": "2018-10-25T06:11:03.000Z"
+    }
+  ]
